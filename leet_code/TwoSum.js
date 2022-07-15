65e8a8ffc53a20fe0833866a71def8bcbfdd0db7
Maurice <maurice.baez1@gmail.com>
2022-07-15 08:26:07 -0700

    solved twoSum

---
+++ leet_code/TwoSum.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
+const twoSum = (nums, target) => {
+  let numsMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let difference = target - nums[i];
+
+    if (numsMap[difference]) {
+      return [numsMap[difference], i];
+    }
+    numsMap[nums[i]] = i;
+  }
+};
