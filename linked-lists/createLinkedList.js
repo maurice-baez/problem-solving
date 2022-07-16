437742abcb82d9a521f976216343edc1d14986da
Maurice <maurice.baez1@gmail.com>
2022-07-16 08:07:18 -0700

    refactored createLinkedList

--- linked-lists/createLinkedList.js
+++ linked-lists/createLinkedList.js
@@ -3,19 +3,12 @@
 /** Write a function, createLinkedList, that takes in an array of values as an argument. The function should create a linked list containing each element of the array as the values of the nodes. The function should return the head of the linked list. */
 
 const createLinkedList = (values) => {
-  let head = null;
-  let current = head;
+  let dummyHead = new Node(null);
+  let tail = dummyHead;
 
   for (let val of values) {
-    let node = new Node(val);
-    if (!head) {
-      head = node;
-      current = head;
-    } else {
-      current.next = node;
-      current = current.next;
-    }
+    tail.next = new Node(val);
+    tail = tail.next;
   }
-
-  return head;
+  return dummyHead.next;
 };
