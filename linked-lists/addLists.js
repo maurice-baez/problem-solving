d5501ad2dc90aa2d81cabba434ebc96186aaba92
Maurice <maurice.baez1@gmail.com>
2022-07-16 08:39:51 -0700

    solved addLists

---
+++ linked-lists/addLists.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** Write a function, addLists, that takes in the head of two linked lists, each representing a number. The nodes of the linked lists contain digits as values. The nodes in the input lists are reversed; this means that the least significant digit of the number is the head. The function should return the head of a new linked listed representing the sum of the input lists. The output list should have its digits reversed as well.*/
+
+const addLists = (head1, head2) => {
+  let num1 = [];
+  let num2 = [];
+  let current1 = head1;
+  let current2 = head2;
+
+  while (current1) {
+    num1.unshift(current1.val);
+    current1 = current1.next;
+  }
+
+  while (current2) {
+    num2.unshift(current2.val);
+    current2 = current2.next;
+  }
+
+  let sum = (Number(num1.join("")) + Number(num2.join(""))).toString();
+  let dummyHead = new Node(null);
+  let tail = dummyHead;
+
+  for (let i = sum.length - 1; i >= 0; i--) {
+    tail.next = new Node(Number(sum[i]));
+    tail = tail.next;
+  }
+  return dummyHead.next;
+};
