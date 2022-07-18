576c7df8e0e0fa16251b4c3a49cc1307cc23c9f2
Maurice <maurice.baez1@gmail.com>
2022-07-17 23:24:07 -0700

    refactored validAnagram

--- leet_code/easy/validAnagram.js
+++ leet_code/easy/validAnagram.js
@@ -9,24 +9,19 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 const freqCounter = (str) => {
   let counter = {};
 
-  for (let ltr of str) {
-    if (counter[ltr]) {
-      counter[ltr]++;
-    } else {
-      counter[ltr] = 1;
-    }
+  for (let char of str) {
+    counter[char] = counter[char] + 1 || 1;
   }
   return counter;
 };
 
 const isAnagram = (str1, str2) => {
   if (str1.length !== str2.length) return false;
+  const str1Counter = freqCounter(str1);
 
-  str1Counter = freqCounter(str1);
-  str2Counter = freqCounter(str2);
-
-  for (let ltr in str1Counter) {
-    if (str1Counter[ltr] !== str2Counter[ltr]) return false;
+  for (let char of str2) {
+    if (!str1Counter[char]) return false;
+    str1Counter[char]--;
   }
   return true;
 };
