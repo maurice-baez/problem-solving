ccb3f8b561c26eb8b3a026e59fe1750ab7b035bd
Maurice <maurice.baez1@gmail.com>
2022-07-18 15:09:18 -0700

    refactored isPalindrome

--- leet_code/easy/isPalindrome.js
+++ leet_code/easy/isPalindrome.js
@@ -4,9 +4,24 @@
 
 Given a string s, return true if it is a palindrome, or false otherwise. */
 
+// const isPalindrome = (str) => {
+//   const regex = /[^A-Za-z0-9]/g;
+//   const parsedStr = str.toLowerCase().replace(regex, "", " ", "");
+
+//   return parsedStr === Array.from(parsedStr).reverse().join("");
+// };
+
 const isPalindrome = (str) => {
   const regex = /[^A-Za-z0-9]/g;
   const parsedStr = str.toLowerCase().replace(regex, "", " ", "");
 
-  return parsedStr === Array.from(parsedStr).reverse().join("");
+  let i = 0;
+  let j = parsedStr.length - 1;
+
+  while (i < j) {
+    if (parsedStr[i] !== parsedStr[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
 };
