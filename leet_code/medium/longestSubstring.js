9f151b179b3cf2ba592f1164dbf1913df94867df
Maurice <maurice.baez1@gmail.com>
2022-07-27 10:20:36 -0700

    solved longestSubstring

---
+++ leet_code/medium/longestSubstring.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/** Given a string str, find the length of the longest substring without repeating characters. */
+
+// two pointers
+// initialize i @ begining of array, j @ i + 1
+// initialize a max variable that will be returned at the end
+
+// begin iterating through string
+// initialize empty set
+// check that chars at i and j are not in set
+// if not --> add them to set, increment j by 1
+// if they are --> increment i to j and j ++
+//check currStreak against max
+//return max
+
+const longestSubstring = (str) => {
+  let i = 0;
+  let j = 1;
+
+  let max = 0;
+
+  while (j < str.length) {
+    let currStreak = 0;
+    let set = new Set(str[i]);
+
+    while (!set.has(str[j]) && j < str.length) {
+      set.add(str[j]);
+      j++;
+    }
+    currStreak = j - i;
+    max = Math.max(currStreak, max);
+    i = j;
+    j++;
+  }
+  return max;
+};
